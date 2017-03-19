060fee6fb78f74b9bb5559fbae3a2c81d10c4230
Kashirin Maxim <ksh.max@gmail.com>
2017-03-19 19:29:08 +0300

    Разные конфиги

---
+++ src/main/java/com/kvteam/backend/LocalConfiguration.java
@@ -0,0 +1,16 @@
+package com.kvteam.backend;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by maxim on 19.03.17.
+ */
+@Profile("default")
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
+public class LocalConfiguration {
+}
