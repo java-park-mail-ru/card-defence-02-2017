97f53d065791807a14e59634b82fc546819e133e
Kashirin Maxim <ksh.max@gmail.com>
2017-03-25 01:07:48 +0300

    Выпилены сокеты

--- src/main/java/com/kvteam/backend/AppConfiguration.java
+++ src/main/java/com/kvteam/backend/AppConfiguration.java
@@ -1,8 +1,5 @@
 package com.kvteam.backend;
 
-import com.kvteam.backend.services.AccountService;
-import com.kvteam.backend.services.MatchmakingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
@@ -15,9 +12,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 import javax.sql.DataSource;
 
@@ -26,16 +20,11 @@ import javax.sql.DataSource;
  */
 @ComponentScan
 @Configuration
-@EnableWebSocket
 @EnableAutoConfiguration
 @ComponentScan
-public class AppConfiguration implements WebSocketConfigurer {
+public class AppConfiguration{
     @Value("${origin}")
     private String origin;
-    @Autowired
-    private GameWebSocketHandler webSocketHandler;
-    @Autowired
-    private AccountService accountService;
 
 
     @Bean
@@ -60,17 +49,6 @@ public class AppConfiguration implements WebSocketConfigurer {
         return DataSourceBuilder.create().build();
     }
 
-    @Bean
-    public CheckCredentialsWebsocketInterceptor credentialsWebsocketInterceptor() {
-        return new CheckCredentialsWebsocketInterceptor(accountService);
-    }
 
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry
-                .addHandler(webSocketHandler, "/connect")
-                .addInterceptors(credentialsWebsocketInterceptor())
-                .setAllowedOrigins(origin);
-    }
 
 }
